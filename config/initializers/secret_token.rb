bb8b3692a64f1af72a53ff0972d068ca8ec548d7
David Gumbiner <dagumbiner@gmail.com>
2012-07-23 18:32:10 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lend::Application.config.secret_token = '2687cc6a22e31fed32e6fa228b56c00568db19de5fe01896fe7a97c5f7ed5cb4aa67ea189cbfed3a608cb9eec122683f36f983117841631d5108ef9c9141b6a9'
