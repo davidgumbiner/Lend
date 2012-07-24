f023fbe930b4522876f3ec42e017e1419db5ba7f
David Gumbiner <dagumbiner@gmail.com>
2012-07-24 14:36:33 -0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Lend::Application.routes.draw do
   resources :users
-  
+  resources :sessions, only: [:new, :create, :destroy]
+
   root to: 'static_pages#home'
 
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/signup',  to: 'users#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
